4ea86239b5683944434ccaa493a80735e03a4bb2
adriannamckeown <71573922+adriannamckeown@users.noreply.github.com>
2021-03-18 19:11:14 -0400

    port 4000

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/appliance.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
