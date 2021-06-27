7dd39975a7f354a431d3e141cf952c6ff0d12fe9
amckeow3 <71570174+amckeow3@users.noreply.github.com>
2021-06-27 14:15:44 -0400

    Update notification.controller.js

--- app/controllers/notification.controller.js
+++ app/controllers/notification.controller.js
@@ -5,11 +5,12 @@ const Item = db.item;
 const mongoose = require('mongoose');
 
 var jwt = require("jsonwebtoken");
+
 exports.newNotification = (req, res) => {
     User.findOne({ username: req.params.username }, (err, user) => {
-            var { item_name, from_dt, to_dt } = req.body;
+            var { item_name, borrower, fromDt, toDt } = req.body;
         User.updateOne({ username: user.username }, 
-            { $push: { notifications: { item_name, from_dt, to_dt } } }, { safe: true, new: true }, (err, user) => {
+            { $push: { notifications: { item_name, borrower, fromDt, toDt } } }, { safe: true, new: true }, (err, user) => {
             if (err) {
                 return res.status(500).send(err);
             } else {
@@ -45,16 +46,17 @@ exports.getNotifications = (req, res) => {
       {
         "$group": {
           "_id": "$notifications._id",
-          "item_name": {$first: "$notifications.item_name"},
-          "from_dt": {$first: "$notifications.from_dt" },
-          "to_dt": {$first: "$notifications.to_dt" },
+          "item_name": { $first: "$notifications.item_name" },
+          "borrower": { $first: "$notifications.borrower" },
+          "fromDt": { $first: "$notifications.fromDt" },
+          "toDt": { $first: "$notifications.toDt" },
           "username": { $first: "$username" },
           "first_name": { $first: "$first_name" },
           "last_name": { $first: "$last_name" },
           "email": { $first: "$email" }
           }
       },
-      { "$project": { "_id": 1, "item_name": 1, "from_dt": 1, "to_dt": 1,  "username": 1, "first_name": 1, "last_name": 1, "email": 1 }}
+      { "$project": { "_id": 1, "item_name": 1, "borrower": 1, "fromDt": 1, "toDt": 1,  "username": 1, "first_name": 1, "last_name": 1, "email": 1 }}
       ], (err, data) => {
       if (err) {
         console.log(err);
